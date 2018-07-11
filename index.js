ce354752ff0a7f7deafe49591e03cbe3d2342cbc
Adriano Rodrigues <adrianomrodrigues@hotmail.com>
2018-07-11 16:16:28 -0300

    adicionando busca de dados atraves de querys

--- index.js
+++ index.js
@@ -7,37 +7,73 @@
         storageBucket: "fir-fast-start.appspot.com",
         messagingSenderId: "926723473603"
     };
+    //initializa o firebase com as configuracoes do projeto definidas na variavel config
+    //para utilizar o firebase, eh necessário ter um projeto cadastrado no console do firebase acessivel por: (https://console.firebase.google.com/)
     firebase.initializeApp(config);
     var liAppDescription = document.getElementById('app-description');
     var h3Users = document.getElementById('users');
-    var firebaseDB = firebase.database().ref().child('users');
+    //pega a instancia do realtime database
+    var firebaseDB = firebase.database();
+    //pega uma referencia ao caminho /users
+    //essa referencia pode ser usada para operacaoes de crud neste objeto
+    var usuariosDB = firebaseDB.ref().child('users');
+    //pega um json criado manualmente para fazer carga no banco a partir dele
     var usersdata = JSON.parse(data);
-
     var usuarioID = 1;
-    console.log(usersdata);
+    //percorre os usurios salvos no json e adiciona eles no realtime database do firebase atraves do metodo set
     for(usuarioID in usersdata){
         usuario = usersdata[usuarioID];
-        firebase.database().ref('users/' + usuarioID).set({
+        firebaseDB.ref('users/' + usuarioID).set({
             username: usuario.name,
             email: usuario.email
             //profile_picture : imageUrl
         });
         usuarioID++;
     }
-    firebaseDB.on('value', changeFunction => {
-        //liAppDescription.innerText = changeFunction.val();
+
+    
+    //Metodo ON eh chamado sempre que os dados sao alterados no banco
+    //Fazendo consulta simples atraves do metodo ON
+    usuariosDB.on('value', changeFunction => {
         var users = changeFunction.val();
         for(userId in users){
             var user = users[userId];
-            console.log(user);
             h3Users.innerHTML += user.username+'<br>';
         }
-        console.log(changeFunction.val());
     });
-    firebaseDB = firebase.database().ref().child('text');
-    firebaseDB.on('value', changeFunction => {
+
+
+    var h3UsersPaginados = document.getElementById('users-paginados');
+    //criando a query para paginar
+    var query = usuariosDB.orderByKey().limitToFirst(3);
+    //filtrando primeiros tres registros ordenados por chave
+    query.on('value', changeFunction => {
+        var users = changeFunction.val();
+        for(userId in users){
+            var user = users[userId];
+            h3UsersPaginados.innerHTML += user.username+'<br>';
+        }
+    });
+
+    var h3UsersPaginadosOrdenadoPorNome = document.getElementById('users-por-nome-paginados');
+    var queryOrdenadaPorNomes = usuariosDB.orderByChild('username').startAt('').limitToFirst(3);
+    //filtrando primeiros 3 registros em ordem alfabetica
+    queryOrdenadaPorNomes.on('value', changeFunction => {
+        var users = changeFunction.val();
+        console.log(users);
+        //retorno da funcao eh em ordem de codigo, assim, eh preciso ordenar os dados para mostra na tela em ordem alfabetica
+        users.sort(function(a,b) {
+            return a.username < b.username ? -1 : a.username > b.username ? 1 : 0;
+        });
+        for(userId in users){
+            var user = users[userId];
+            h3UsersPaginadosOrdenadoPorNome.innerHTML += user.username+'<br>';
+        }
+    });
+    //cria referencia ao objeto text, um objeto padrao para trazer a descricao do site
+    var textDB = firebase.database().ref().child('text');
+    textDB.on('value', changeFunction => {
         liAppDescription.innerText = changeFunction.val();
-        console.log(changeFunction.val());
     });
     
 
